75aaf8a8c378a4dc48a85c31b64956e9207baa04
Rajnigandha Jadhav <jadhavrajnigandha@gmail.com>
2022-09-19 08:23:41 +0530

    commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,14 +4,7 @@ const router = express.Router();
 const collegeController = require("../controllers/collegeController")
 const internController = require("../controllers/createInternValidator")
 
-//------------------------------------------> (This is test api ) <--------------------------------------------//
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-
-// ===================================================( All api)======================================================///
+/// ===================================================( All api)======================================================///
 
 
 //-------------------------> (When college creat, call this api) <----------------------------------//
